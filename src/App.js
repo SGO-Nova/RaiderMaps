99e542ee049595b96b7fccdce28edf2c7f4334ce
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2021-02-07 02:29:22 -0600

    Dot stays in same location no matter the monitor aspect ratio now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Switch from "react-switch";
-import React, {Component}from 'react';
-import TextField from "@material-ui/core/TextField"
+import React, {Component, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import './App.css';
@@ -210,12 +210,11 @@ const HTMLTooltip = withStyles((theme) => ({
 const data = require('./locations.json');
 var Buildings = [];
 
-for(var i = 0; i < data.Buildings.length; i++){
-  Buildings.push(data.Buildings[i][0]);
-}
 
+
+//function for settings tab; class adding for animations
 function remove(input){
-  if(input == true){
+  if(input === true){
     document.getElementById("settingsTab").style.left = 0;
     document.getElementById("settingsTab").classList.remove('horizTranslateInto');
   }
@@ -226,39 +225,72 @@ function remove(input){
   
 }
 
-function remove2(input){
-  if(input == true){
-    document.getElementById("main1").style.height = 150 + 'px';
-    document.getElementById("main1").classList.remove('verticalExpand');
+
+
+//Function to keep the div holding the dots at the same ratio and location as the picture 
+window.addEventListener("resize", () => {
+  var w = 0; //width
+  var h = 0; //height
+  var mL = 0; //marginLeft
+  var mT = 0; //marginTop
+
+  if(window.innerWidth / window.innerHeight > (5/3)){
+    //Wider than it needs to be
+    h = window.innerHeight;
+    w = (window.innerHeight * (5/3));
+    mL = (window.innerWidth - w) / 2;
   }
   else{
-    document.getElementById("main1").style.height = 50 + 'px';
-    document.getElementById("main1").classList.remove('verticalColapse');
+    //Taller than it needs to be
+    w = window.innerWidth;
+    h = (window.innerWidth * .6);
+    mT = (window.innerHeight - h) / 2;
   }
-}
+
+  console.log("H: " + h + " W: " + w);
+  document.getElementById("dotHolder").style.height = h + "px";
+  document.getElementById("dotHolder").style.width = w + "px";
+  document.getElementById("dotHolder").style.marginTop = mT + "px";
+  document.getElementById("dotHolder").style.marginLeft = mL + "px";
+  console.log("H: " + h + " W: " + w + " mT: " + mT + " mL: " + mL);
+});
+
 
 const arrow1 = new tab("up", 150, 50);
 
+//Sidepanel that has settings button, search bar, and find button
 const side1 = new SidePanel(50, SET_WIDTH, 
   <div className="sidePanelInside">
-    <HTMLTooltip title="Settings" placement="bottom" TransitionComponent={Zoom}>
-      <button className="searchButton1" onClick={() => {
-        document.getElementById("settingsTab").classList.add('horizTranslateInto');
-        setTimeout(() => {
-          remove(true)}, 1500); 
-      }}>
-        <img src={settings} alt="Settings button"/>
-      </button>
-    </HTMLTooltip>
-    <TextField placeholder="Search a Location..." style={{width:"270px"}} InputProps={{ disableUnderline: true, style:{fontSize: 20}}} inputProps={{style:{padding: 0}}}/>
-    <HTMLTooltip title="Locate" placement="bottom" TransitionComponent={Zoom}>
-      <button className="searchButton2">
-        <img src={locate} alt="Locate location button"/>
-      </button>
-    </HTMLTooltip>
+    <div style={{float: "left"}}>
+      <HTMLTooltip title="Settings" placement="bottom" TransitionComponent={Zoom}>
+        <button className="searchButton1" onClick={() => {
+          document.getElementById("settingsTab").classList.add('horizTranslateInto');
+          setTimeout(() => {
+            remove(true)}, 1500); 
+        }}>
+          <img src={settings} alt="Settings button"/>
+        </button>
+      </HTMLTooltip>
+    </div>
+    
+    {/*TextField for location finding */}
+    <div style={{width: "270px", float: "left"}}>
+      <Autocomplete freeSolo  options={Buildings}  renderInput={(parms) => (<TextField {...parms} InputProps={{ ...parms.InputProps, disableUnderline: true, style:{fontSize: 20}}} inputProps={{...parms.inputProps, style:{margin:"-4px"}}} id="textLocator" placeholder="Search a Location..." />)} /> {/* style={{width:"270px"}}   */}
+    </div>
+    <div style={{float: "left"}}>
+      <HTMLTooltip title="Locate" placement="bottom" TransitionComponent={Zoom}>
+        <button className="searchButton2" onClick={() => {
+          
+        }}>
+          <img src={locate} alt="Locate location button"/>
+        </button>
+      </HTMLTooltip>
+    </div>
   </div>
 );
 
+
+//Side Panel that holds side panel 1 and common places and locations buttons
 const side2 = new SidePanel(250, SET_WIDTH,
   <div id="main1">
     {side1.renderNoMargin()}
@@ -278,7 +310,36 @@ const side2 = new SidePanel(250, SET_WIDTH,
 
 
 function App(){
+  useEffect(() => {
+    for(var i = 0; i < data.Buildings.length; i++){
+      Buildings.push(data.Buildings[i][0]);
+    }
 
+    var w = 0; //width
+    var h = 0; //height
+    var mL = 0; //marginLeft
+    var mT = 0; //marginTop
+  
+    if(window.innerWidth / window.innerHeight > (5/3)){
+      //Wider than it needs to be
+      h = window.innerHeight;
+      w = (window.innerHeight * (5/3));
+      mL = (window.innerWidth - w) / 2;
+    }
+    else{
+      //Taller than it needs to be
+      w = window.innerWidth;
+      h = (window.innerWidth * .6);
+      mT = (window.innerHeight - h) / 2;
+    }
+
+    document.getElementById("dotHolder").style.height = h + "px";
+    document.getElementById("dotHolder").style.width = w + "px";
+    document.getElementById("dotHolder").style.marginTop = mT + "px";
+    document.getElementById("dotHolder").style.marginLeft = mL + "px";
+    console.log("H: " + h + " W: " + w + " mT: " + mT + " mL: " + mL);
+  }, []);
+  
   return (
     <body>
         
@@ -295,59 +356,65 @@ function App(){
               <React.Fragment>
                 <TransformComponent >
                   <div className="Container" >
-                    <img src={TTUMap} alt="TTU Map" className="mapImage" /> {/*REPLACE demo with TTUMap for final product*/}
+                    <img src={TTUMap} alt="TTU Map" className="mapImage"/> {/*REPLACE demo with TTUMap for final product*/}{/*Image size stays the same as 100vh and 100vw */}
+                    <div id="dotHolder"  style={{position: "absolute", top: "0",}}>
+                      <div id="locationDot" style={{position: "absolute", height:"1mm", width:"1mm", backgroundColor:"red", top: "45%", left: "30%"}}>
+                        {/*Test div for location placing */}
+                      </div>
+                    </div>
+                    
                   </div>
                 </TransformComponent>
                 
                 {/*Map Buttons */}
                 <div className="mapButtonsContainer">
-                      <HTMLTooltip title="Zoom In" placement="left" TransitionComponent={Zoom}>
-                        <button
-                          onClick={zoomIn}
-                          className="mapButtons"
-                        >
-                          <img src={zoom_in} alt="" />
-                        </button>
-                      </HTMLTooltip>
-                      <HTMLTooltip title="Zoom Out" placement="left" TransitionComponent={Zoom}>
-                      <button
-                        onClick={zoomOut}
-                        className="mapButtons"
-                      >
-                        <img src={zoom_out} alt="" />
-                      </button>
-                      </HTMLTooltip>
-                      <HTMLTooltip title="Zoom Reset" placement="left" TransitionComponent={Zoom}>
-                      <button
-                        onClick={resetTransform}
-                        className="mapButtons"
-                      >
-                        <img src={zoom_reset} alt="" />
-                      </button>
-                      </HTMLTooltip>
+                  <HTMLTooltip title="Zoom In" placement="left" TransitionComponent={Zoom}>
+                    <button
+                      onClick={zoomIn}
+                      className="mapButtons"
+                    >
+                      <img src={zoom_in} alt="" />
+                    </button>
+                  </HTMLTooltip>
+                  <HTMLTooltip title="Zoom Out" placement="left" TransitionComponent={Zoom}>
+                  <button
+                    onClick={zoomOut}
+                    className="mapButtons"
+                  >
+                    <img src={zoom_out} alt="" />
+                  </button>
+                  </HTMLTooltip>
+                  <HTMLTooltip title="Zoom Reset" placement="left" TransitionComponent={Zoom}>
+                  <button
+                    onClick={resetTransform}
+                    className="mapButtons"
+                  >
+                    <img src={zoom_reset} alt="" />
+                  </button>
+                  </HTMLTooltip>
+                </div>
+                <div className="sidePanelContainer">
+                  {side2.renderMargin()}
+                </div>
+                <div id="settingsTab">
+                  <div style={{marginBottom:"16px"}}>Settings</div>
+                  <div style={{position:"absolute", top: "8px", right: "8px"}}><button className="searchButton1" onClick={() => {
+                    document.getElementById("settingsTab").classList.add('horizTranslateOut');
+                    setTimeout(() => {
+                      remove(false)}, 1500); 
+                    }
+                  }><img src={back} alt="Minimize settings screen"/></button></div>
+                  <div className="settingsSwitches">
+                    <div className="settings">
+                      <p style={{marginRight:"10px"}}>Theme:</p>
+                      <ThemeSwitch/>
+                    </div> 
+                    <div className="settings">
+                      <p style={{marginRight:"8px"}}>Bathroom:</p>
+                      <BathroomSwitch/>
                     </div>
-                    <div className="sidePanelContainer">
-                      {side2.renderMargin()}
-                    </div>
-                    <div id="settingsTab">
-                      <div style={{marginBottom:"16px"}}>Settings</div>
-                      <div style={{position:"absolute", top: "8px", right: "8px"}}><button className="searchButton1" onClick={() => {
-                        document.getElementById("settingsTab").classList.add('horizTranslateOut');
-                        setTimeout(() => {
-                          remove(false)}, 1500); 
-                        }
-                      }><img src={back} alt="Minimize settings screen"/></button></div>
-                      <div className="settingsSwitches">
-                        <div className="settings">
-                          <p style={{marginRight:"10px"}}>Theme:</p>
-                          <ThemeSwitch/>
-                        </div> 
-                        <div className="settings">
-                          <p style={{marginRight:"8px"}}>Bathroom:</p>
-                          <BathroomSwitch/>
-                        </div>
-                      </div>
-                    </div>
+                  </div>
+                </div>
               </React.Fragment>
               )}
         </TransformWrapper>
